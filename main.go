d960eaba204c59935afdd10d106c4ea1cc1c4be1
fahmiazis <fahmiazis797@gmail.com>
2026-02-11 12:31:27 +0700

    update cors

--- main.go
+++ main.go
@@ -27,7 +27,8 @@ func main() {
 
 	// Setup CORS
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173"},
+		// AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173"},
+		AllowAllOrigins:  true,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
